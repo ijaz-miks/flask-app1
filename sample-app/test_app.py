ea553fdb1ae319a5d159a799b008c5d748c5db90
muhammed.ijaz <muhammed.ijaz@scandiweb.com>
2025-01-01 11:31:15 +0200

    THES-2: Updating the unit test.

--- sample-app/test_app.py
+++ sample-app/test_app.py
@@ -8,7 +8,7 @@ class TestApp(unittest.TestCase):
     def test_hello(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode(), "Hello, World from Flask!")
+        self.assertEqual(response.data.decode(), "Welcome to the Online Store API Gateway!")
 
 if __name__ == '__main__':
     unittest.main()
